1fba8a8677535a35ee0d6dfde400c402f8af585f
Oli Nelson <olivernelson@icloud.com>
2020-02-05 17:33:52 -0500

    gatsby node path

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,8 @@ exports.onCreatePage = async ({ page, actions }) => {
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
   if (page.path.match(/^\/app/)) {
-    page.matchPath = "/app/*"
+    page.matchPath = "/transcribr2/app/*"
+    // page.matchPath = "/app/*"
 
     // Update the page.
     createPage(page)
