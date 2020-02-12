7ea52228d7bd4f3c604fb05a7a1b546f6bbf2b56
Oli Nelson <olivernelson@icloud.com>
2020-02-12 11:14:50 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-78c0630a9a198c28fc86.js"
+    "url": "webpack-runtime-b794168bd5003a7387c3.js"
   },
   {
     "url": "styles.4bdd84f921aeff082b4a.css"
@@ -36,20 +36,20 @@ self.__precacheManifest = [
     "url": "styles-2a32473c7005e4bc8845.js"
   },
   {
-    "url": "commons-1d852c17d6415e1cb5e2.js"
+    "url": "commons-bc58521c486fcedf9c51.js"
   },
   {
-    "url": "app-4ed124d7fb8679953314.js"
+    "url": "app-647654bb6f09dfe50608.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-5d3e3dbea3f6c92bbad1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0174af44eef21410f597dd9962005f07"
+    "revision": "8d3ae2cd6f274340691d490986002ff3"
   },
   {
-    "url": "component---src-pages-app-js-a2cecebf5dddde599152.js"
+    "url": "component---src-pages-app-js-667303bbecf75e9d0eab.js"
   },
   {
     "url": "page-data/app/page-data.json",
@@ -150,7 +150,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-4ed124d7fb8679953314.js`))) {
+  if (!resources || !(await caches.match(`/app-647654bb6f09dfe50608.js`))) {
     return await fetch(event.request)
   }
 
