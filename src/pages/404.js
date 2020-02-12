583f837f5b397f5769f4b57a2a371bd2dadb7c20
Oli Nelson <olivernelson@icloud.com>
2020-02-12 11:13:40 -0500

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+import { useStaticQuery, graphql, navigate } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+
+export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "images/wordcloudBlue.png" }) {
+        childImageSharp {
+          # Specify a fluid image and fragment
+          # The default maxWidth is 800 pixels
+          fluid(fit: CONTAIN) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <div
+        style={{
+          display: "grid",
+          justifySelf: "center",
+          alignSelf: "center",
+          justifyContent: "center",
+          width: "100%",
+          maxWidth: "40rem",
+          textAlign: "center",
+        }}
+      >
+        <Img fluid={data.file.childImageSharp.fluid} alt="Transcribr Logo" />
+        <h1
+          style={{
+            color: "#1890FF",
+            textAlign: "center",
+            fontSize: "10vw",
+          }}
+        >
+          404
+        </h1>
+        <p>Hmm... sorry the page you are looking for can't be found.</p>
+        <a onClick={() => navigate("/")}>Take me home</a>
+      </div>
+    </Layout>
+  )
+}
