a1dd523318991190a484c369797ee227201aa4d8
Oli Nelson <olivernelson@icloud.com>
2020-02-15 11:17:24 -0500

    edit clip form cleanup

--- src/components/EditClipDrawer.js
+++ src/components/EditClipDrawer.js
@@ -10,14 +10,18 @@ import {
   Icon,
   DatePicker,
   Divider,
+  Collapse,
 } from "antd"
 
 import { updateClip } from "../services/clipManagement"
+import ButtonGroup from "antd/lib/button/button-group"
+import CollapsePanel from "antd/lib/collapse/CollapsePanel"
 
+const { Panel } = Collapse
 const EditClipDrawer = props => {
   const { updateClipInProfile, deleteClipHandler } = props
   const editing = props.clip.editing
-  const [clip, setClip] = useState(props.clip)
+  const [formData, setFormData] = useState(props.clip)
 
   return (
     <Drawer
@@ -26,8 +30,8 @@ const EditClipDrawer = props => {
       placement="right"
       closable={true}
       onClose={() => {
-        updateClip(clip, updateClipInProfile, setClip)
-        props.setClip({ ...clip, editing: false })
+        // updateClip(clip, updateClipInProfile, setClip)
+        props.setClip({ ...props.clip, editing: false })
       }}
       visible={editing}
     >
@@ -35,158 +39,177 @@ const EditClipDrawer = props => {
         layout="vertical"
         onChange={e => {
           if (e.target.name === "name")
-            setClip({ ...clip, [e.target.name]: e.target.value })
+            setFormData({ ...formData, [e.target.name]: e.target.value })
           else
-            setClip({
-              ...clip,
-              citation: { ...clip.citation, [e.target.name]: e.target.value },
+            setFormData({
+              ...formData,
+              citation: {
+                ...formData.citation,
+                [e.target.name]: e.target.value,
+              },
             })
         }}
         onSubmit={e => {
           e.preventDefault()
-          updateClip(clip, updateClipInProfile, setClip)
+          updateClip(formData, updateClipInProfile, props.setClip)
         }}
       >
         <Form.Item label="Clip Name">
-          <Input name="name" spellCheck="true" defaultValue={clip.name} />
-        </Form.Item>
-        <h2>Citation</h2>
-        <Divider />
-
-        <Form.Item label="First Name">
-          <Input
-            name="firstName"
-            spellCheck="true"
-            defaultValue={clip.citation.firstName}
-            placeholder="Dave"
-          />
-        </Form.Item>
-        <Form.Item label="Middle Initial">
-          <Input
-            name="middleInitial"
-            spellCheck="true"
-            defaultValue={clip.citation.middleInitial}
-            placeholder="D"
-          />
+          <Input name="name" spellCheck="true" defaultValue={formData.name} />
         </Form.Item>
 
-        <Form.Item label="Last Name">
-          <Input
-            name="lastName"
-            spellCheck="true"
-            defaultValue={clip.citation.lastName}
-            placeholder="Varialle"
-          />
-        </Form.Item>
-        <Form.Item label="Contributor Title">
-          <Input
-            name="contributorTitle"
-            spellCheck="true"
-            defaultValue={clip.citation.contributorTitle}
-            placeholder="host"
-          />
-        </Form.Item>
+        <Collapse>
+          <Panel header="Citation">
+            <Form.Item label="First Name">
+              <Input
+                name="firstName"
+                spellCheck="true"
+                defaultValue={formData.citation.firstName}
+                placeholder="Dave"
+              />
+            </Form.Item>
+            <Form.Item label="Middle Initial">
+              <Input
+                name="middleInitial"
+                spellCheck="true"
+                defaultValue={formData.citation.middleInitial}
+                placeholder="D"
+              />
+            </Form.Item>
 
-        <Divider />
+            <Form.Item label="Last Name">
+              <Input
+                name="lastName"
+                spellCheck="true"
+                defaultValue={formData.citation.lastName}
+                placeholder="Varialle"
+              />
+            </Form.Item>
+            <Form.Item label="Contributor Title">
+              <Input
+                name="contributorTitle"
+                spellCheck="true"
+                defaultValue={formData.citation.contributorTitle}
+                placeholder="host"
+              />
+            </Form.Item>
 
-        <Form.Item label="Media Description">
-          <Input
-            name="mediaDescription"
-            spellCheck="true"
-            defaultValue={clip.citation.mediaDescription}
-            placeholder="Audio Podcast"
-          />
-        </Form.Item>
+            <Divider />
 
-        <Form.Item label="Show/Podcast Title">
-          <Input
-            name="showTitle"
-            spellCheck="true"
-            defaultValue={clip.citation.showTitle}
-            placeholder="I'd Hit That"
-          />
-        </Form.Item>
-        <Form.Item label="Episode Title">
-          <Input
-            name="episodeTitle"
-            spellCheck="true"
-            defaultValue={clip.citation.episodeTitle}
-            placeholder="102 - Dan Weiss"
-          />
-        </Form.Item>
-        <Form.Item label="Publisher">
-          <Input
-            name="publisher"
-            spellCheck="true"
-            defaultValue={clip.citation.publisher}
-            placeholder="Podomatic.net"
-          />
-        </Form.Item>
+            <Form.Item label="Media Description">
+              <Input
+                name="mediaDescription"
+                spellCheck="true"
+                defaultValue={formData.citation.mediaDescription}
+                placeholder="Audio Podcast"
+              />
+            </Form.Item>
 
-        <Divider />
+            <Form.Item label="Show/Podcast Title">
+              <Input
+                name="showTitle"
+                spellCheck="true"
+                defaultValue={formData.citation.showTitle}
+                placeholder="I'd Hit That"
+              />
+            </Form.Item>
+            <Form.Item label="Episode Title">
+              <Input
+                name="episodeTitle"
+                spellCheck="true"
+                defaultValue={formData.citation.episodeTitle}
+                placeholder="102 - Dan Weiss"
+              />
+            </Form.Item>
+            <Form.Item label="Publisher">
+              <Input
+                name="publisher"
+                spellCheck="true"
+                defaultValue={formData.citation.publisher}
+                placeholder="Podomatic.net"
+              />
+            </Form.Item>
 
-        <Form.Item label="Date Posted">
-          <DatePicker
-            allowClear={false}
-            defaultValue={moment(clip.citation.datePosted)}
-            onChange={e =>
-              setClip({
-                ...clip,
-                citation: { ...clip.citation, datePosted: e.valueOf() },
-              })
-            }
-          />
-        </Form.Item>
-        <Form.Item label="Date Accessed">
-          <DatePicker
-            allowClear={false}
-            defaultValue={moment(clip.citation.accessed)}
-            onChange={e =>
-              setClip({
-                ...clip,
-                citation: { ...clip.citation, dateAccessed: e.valueOf() },
-              })
-            }
-          />
-        </Form.Item>
-        <Form.Item label="Place Of Recording">
-          <Input
-            name="placeOfRecording"
-            spellCheck="true"
-            defaultValue={clip.citation.placeOfRecording}
-            placeholder="Los Angeles, USA"
-          />
-        </Form.Item>
-        <Form.Item label="Url">
-          <Input
-            name="url"
-            spellCheck="true"
-            defaultValue={clip.citation.url}
-            placeholder="https://www.podomatic.com/podcasts/idhitthatpodcast/episodes/2017-10-22T09_39_47-07_00"
-          />
-        </Form.Item>
+            <Divider />
 
-        <Divider />
+            <Form.Item label="Date Posted">
+              <DatePicker
+                allowClear={false}
+                defaultValue={moment(formData.citation.datePosted)}
+                onChange={e =>
+                  setFormData({
+                    ...formData,
+                    citation: { ...formData.citation, datePosted: e.valueOf() },
+                  })
+                }
+              />
+            </Form.Item>
+            <Form.Item label="Date Accessed">
+              <DatePicker
+                allowClear={false}
+                defaultValue={moment(formData.citation.accessed)}
+                onChange={e =>
+                  setFormData({
+                    ...formData,
+                    citation: {
+                      ...formData.citation,
+                      dateAccessed: e.valueOf(),
+                    },
+                  })
+                }
+              />
+            </Form.Item>
+            <Form.Item label="Place Of Recording">
+              <Input
+                name="placeOfRecording"
+                spellCheck="true"
+                defaultValue={formData.citation.placeOfRecording}
+                placeholder="Los Angeles, USA"
+              />
+            </Form.Item>
+            <Form.Item label="Url">
+              <Input
+                name="url"
+                spellCheck="true"
+                defaultValue={formData.citation.url}
+                placeholder="https://www.podomatic.com/podcasts/idhitthatpodcast/episodes/2017-10-22T09_39_47-07_00"
+              />
+            </Form.Item>
 
-        <Button onClick={() => props.setClip({ ...clip, editing: false })}>
-          Cancel
-        </Button>
+            <Divider />
+          </Panel>
+        </Collapse>
 
-        <Popconfirm
-          title="Are you sure you want to delete this clip?"
-          onConfirm={() => deleteClipHandler(clip._id)}
-          okText="Yes"
-          cancelText="No"
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3,1fr)",
+            gridGap: "1rem",
+            marginTop: "1rem",
+          }}
         >
-          <Button type="danger">
-            <Icon type="delete" />
-            Delete
+          <Button
+            onClick={() => props.setClip({ ...props.clip, editing: false })}
+          >
+            Cancel
           </Button>
-        </Popconfirm>
 
-        <Button type="primary" htmlType="submit" loading={clip.saving}>
-          Save
-        </Button>
+          <Popconfirm
+            title="Are you sure you want to delete this clip?"
+            onConfirm={() => deleteClipHandler(formData._id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="danger">
+              <Icon type="delete" />
+              Delete
+            </Button>
+          </Popconfirm>
+
+          <Button type="primary" htmlType="submit" loading={formData.saving}>
+            Save
+          </Button>
+        </div>
       </Form>
     </Drawer>
   )
