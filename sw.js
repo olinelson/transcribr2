8602f1cb1217c6cddc6fc1e14f02862b7dc3221e
Oli Nelson <olivernelson@icloud.com>
2020-02-16 21:40:43 -0500

    Updates

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-2a32473c7005e4bc8845.js"
   },
   {
-    "url": "commons-a8eb51475fc088de0134.js"
+    "url": "commons-20e3a44478047c7e5480.js"
   },
   {
     "url": "app-cc7101e36c73eeb54c00.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b5a4f3f93df3126ea31772a2b1023e77"
+    "revision": "f21f1fb8351d5a87929172b5e83c86cc"
   },
   {
     "url": "component---src-pages-app-js-c24b9ac21a9547014c1f.js"
