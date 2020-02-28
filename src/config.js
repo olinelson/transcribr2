74b38f745a0fccd706bfe760de2bd065f02383f9
Oli Nelson <olivernelson@icloud.com>
2020-02-28 13:35:58 -0500

    prod

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://transcribr2-api.herokuapp.com'
-export const API_URL = 'https://transcribr2-api-staging.herokuapp.com'
+export const API_URL = 'https://transcribr2-api.herokuapp.com'
+// export const API_URL = 'https://transcribr2-api-staging.herokuapp.com'
 // export const API_URL = 'http://localhost:3000'
 
 export const LanguageOptions = [
