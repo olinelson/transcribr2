dcc4712b3235f06c8361ca87f5544aaeeb845fd6
Oli Nelson <olivernelson@icloud.com>
2020-03-05 12:10:52 -0500

    404 cleanup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react'
-
-import { useStaticQuery, graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
 import Layout from '../components/layout'
-import { Icon, Divider } from 'antd'
+import { Icon } from 'antd'
 
 export default function IndexPage () {
-  return <Icon style={{ fontSize: '5vw', position: 'absolute', top: '50%', left: '50%' }} type='loading' />
+  const [showMessage, setShowMessage] = useState(false)
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setShowMessage(true)
+    }, 5000)
+
+    return function () {
+      clearTimeout(delay)
+    }
+  })
+
+  return (
+    <Layout>
+      <div style={{ fontSize: '5vw', position: 'absolute', top: '50%', width: '100vw', textAlign: 'center' }}>
+
+        {showMessage ? <Icon type='question' /> : <Icon type='loading' />}
+
+      </div>
+    </Layout>
+  )
 }
