d72c102b423d76646189a5d8d6463a0ebb32a1eb
Oli Nelson <olivernelson@icloud.com>
2020-03-07 16:54:25 -0500

    added live stripe key

--- src/components/PaymentMethodsList.js
+++ src/components/PaymentMethodsList.js
@@ -10,7 +10,8 @@ import AddPaymentForm from './AddPaymentForm'
 import { openNotificationWithIcon } from './Notifications'
 import { useStateWithLocalStorageJSON } from '../utils'
 
-const stripePromise = loadStripe('pk_test_9MNFVyvIUuMqQgdozJBdDxjO005OlKPNVa')
+// publishable stripe key
+const stripePromise = loadStripe('pk_live_cXMZDMoPxJaKGOa5MEXk09PU007Ke5wshF')
 
 export default function PaymentMethodsList () {
   const [cards, setCards] = useStateWithLocalStorageJSON('cards', [])
@@ -84,15 +85,15 @@ align-items: start;
             {cards.length <= 1
               ? <Popover content='To delete a card, first add a new one.' trigger='hover'>
                 <Button type='link' icon='delete' disabled />
-              </Popover>
+                </Popover>
               : <Popconfirm
                 title='Are you sure delete this card?'
                 onConfirm={() => deletePaymentMethodHandler(c.id)}
                 okText='Yes'
                 cancelText='No'
-                >
+              >
                 <Icon type='delete' />
-                </Popconfirm>}
+              </Popconfirm>}
 
           </StyledListItem>
         )}
