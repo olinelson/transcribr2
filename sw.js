04859a841a2e6367cef89476d2934ee7ea146ac9
Oli Nelson <olivernelson@icloud.com>
2020-03-09 13:42:38 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d2f98e94d8d0b1060a45.js"
+    "url": "webpack-runtime-dcc70d2f13a08f163423.js"
   },
   {
     "url": "styles.548178f3f127bd7c0471.css"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-2a32473c7005e4bc8845.js"
   },
   {
-    "url": "commons-5d99fbe6428eeef7c414.js"
+    "url": "commons-b8415edfb8fdaa8c0842.js"
   },
   {
     "url": "app-45d7d2b47f85bad7aebb.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e951f889259fd407008fa27ec55b571c"
+    "revision": "cc41a1b5adc2a7d2b456d1ddbb72edaa"
   },
   {
     "url": "component---src-pages-index-js-24f9c048d7735173da42.js"
@@ -70,7 +70,7 @@ self.__precacheManifest = [
     "revision": "78ee27b54d9cd221859de6d91975030b"
   },
   {
-    "url": "component---src-pages-app-js-c60a67d8b8da018f544f.js"
+    "url": "component---src-pages-app-js-2872c2a7e0671e52be36.js"
   },
   {
     "url": "page-data/app/page-data.json",
