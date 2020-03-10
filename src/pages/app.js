b7783cf1c947cf1dd00d2b0b624a2f09aaeb1ef5
Oli Nelson <olivernelson@icloud.com>
2020-03-10 12:34:15 -0400

    update profile on visible

--- src/pages/app.js
+++ src/pages/app.js
@@ -94,6 +94,7 @@ function App (props) {
         // console.log('joining user channel')
         message.success('Back online!')
         socket.emit('joinUserChannel', bearerToken)
+        console.log('getting user profile and setting')
         getUserProfileAndSet(appState, setAppState)
       } else {
         // console.log('leaving user channel')
