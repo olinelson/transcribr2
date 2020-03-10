30e27a3280fcf6262eb61a653cb9a8f841be774c
Oli Nelson <olivernelson@icloud.com>
2020-03-10 15:32:47 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a34b296904ecc20960ea.js"
+    "url": "webpack-runtime-1ecf146cfe5e6012d1b7.js"
   },
   {
     "url": "styles.548178f3f127bd7c0471.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8000eca5bcbc27403ba0e35b781be6f8"
+    "revision": "41c2af89c69a66749c9ebae31dd4cb38"
   },
   {
     "url": "component---src-pages-index-js-d4bd3d348056db366b77.js"
@@ -70,7 +70,7 @@ self.__precacheManifest = [
     "revision": "78ee27b54d9cd221859de6d91975030b"
   },
   {
-    "url": "component---src-pages-app-js-c810cb346c46ef8e9932.js"
+    "url": "component---src-pages-app-js-91e120fc36ee2712e8fd.js"
   },
   {
     "url": "page-data/app/page-data.json",
