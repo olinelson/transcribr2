c0731097dc52dc5c738de6850906cb82933d5f23
Oli Nelson <olivernelson@icloud.com>
2020-04-11 17:47:20 +1000

    added custom cache service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,14 @@
 import './src/styles/global.css'
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. ' +
-        'Reload to display the latest version?'
-  )
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     'This application has been updated. ' +
+//         'Reload to display the latest version?'
+//   )
 
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
 
 export const registerServiceWorker = () => true
